13ce9995c7dd200037f2429ce03e8f813cad75c0
Philipp Zehnder <zehnder@fzi.de>
2018-06-30 18:43:09 +0200

    Map works now

--- visualization/app.js
+++ visualization/app.js
@@ -2,34 +2,41 @@ parameterName  = "event";
 parameterValue = "";
 
 location.search.substr(1).split("&").forEach(function (item)
-{
-	tmp = item.split("=");
-	
-	if (tmp[0] === parameterName)
 	{
-		parameterValue = decodeURIComponent(tmp[1]);
-	}
-});
+		tmp = item.split("=");
+
+		if (tmp[0] === parameterName)
+		{
+			parameterValue = decodeURIComponent(tmp[1]);
+		}
+	});
 
 var myApp = angular.module('myApp', ['angular-maps']);
 
 myApp.controller('Ctrl', ['$scope', '$http', function($scope, $http)
-{
-	if(parameterValue === "")
 	{
-		$http.get('http://localhost:5984/testdb/1090ea098e2b0cdb136a7512a4000019').success(function(data)
-		{
-			$scope.valueRange = [0,100];
-			$scope.colorRange = ["#F03B20", "#FFEDA0"];
-			$scope.dimension = 800;
-			$scope.mapWidth = 1240;
-			$scope.descriptiveText = '';
-			$scope.countryFillColor = "#aaa";
-			$scope.countryBorderColor = "#fff";
-			$scope.worldData = data["countries"];
-		});
-	}
-	else
-	{
-	}
-}]);
+
+		$scope.render = true;
+
+		$http.get('http://localhost:5984/testdb/e83f2c41ef59980f21f3e4ba3c000df1').success(function(data)
+			{
+				$scope.worldData = data["countries"];
+				$scope.render = false;
+
+				setTimeout(function() {
+					$scope.$apply(function () {
+						$scope.render = true;
+					});
+				}, 200);
+			});
+
+		$scope.valueRange = [0,100];
+		$scope.colorRange = ["#F03B20", "#FFEDA0"];
+		$scope.dimension = 800;
+		$scope.mapWidth = 1240;
+		$scope.descriptiveText = '';
+		$scope.countryFillColor = "#aaa";
+		$scope.countryBorderColor = "#fff";
+		$scope.worldData = [];
+
+	}]);
